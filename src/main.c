da77b3b0873cca819f84578d2e7aa06319c884f2
Mateusz <mateusz.zdrod@gmail.com>
2025-11-20 17:36:54 +0100

    start

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "../minilibx-linux/mlx.h"
+
+int main(void)
+{
+    void *mlx;
+    void *mlx_win;
+
+    mlx = mlx_init();
+    mlx_win = mlx_new_window(mlx, 1280, 720, "Hello world!");
+    mlx_loop(mlx);
+}
