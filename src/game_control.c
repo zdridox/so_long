0fa0a11f1eddf530c723d55aa86c9903a0d93d70
Mateusz <mateusz.zdrod@gmail.com>
2025-11-22 00:30:45 +0100

    exit_not_walkable

--- src/game_control.c
+++ src/game_control.c
@@ -7,7 +7,7 @@ void move_player(t_map *map, t_data *data, int x, int y)
     player_pos = get_player_cords(map);
     if ((player_pos->x + x > map->size_x || player_pos->x + x < 0) || (player_pos->y + y > map->size_y || player_pos->y + y < 0))
         return;
-    if (map->map[player_pos->y + y][player_pos->x + x] != '1')
+    if (map->map[player_pos->y + y][player_pos->x + x] != '1' && map->map[player_pos->y + y][player_pos->x + x] != 'E')
     {
         if (map->map[player_pos->y + y][player_pos->x + x] == 'C')
             data->collectibles++;
