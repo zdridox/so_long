a71bee6a70e57f38eff427452e5dbdfded3d6e48
Mateusz <mateusz.zdrod@gmail.com>
2025-11-22 00:36:43 +0100

    gracz popinkala

--- src/map_validator.c
+++ src/map_validator.c
@@ -93,25 +93,25 @@ int validate_map(t_map *map)
     return (result);
 }
 
-int main(int argc, char **argv)
-{
-    t_map *map = parse_map(argv[1]);
-    ft_printf("result: %d\n", validate_map(map));
-    t_cords *player_cords = get_player_cords(map);
-    ft_printf("player cords: (x: %d, y: %d)\n\n", player_cords->x, player_cords->y);
-    t_map *flooded = flood_fill(map, player_cords->x, player_cords->y);
-    str_arr_print(map->map);
-    ft_printf("\n");
-    str_arr_print(flooded->map);
-    if (map)
-        str_arr_free(map->map);
-    free(map);
-    if (flooded)
-        str_arr_free(flooded->map);
-    free(flooded);
-    free(player_cords);
-    return 0;
-}
+// int main(int argc, char **argv)
+// {
+//     t_map *map = parse_map(argv[1]);
+//     ft_printf("result: %d\n", validate_map(map));
+//     t_cords *player_cords = get_player_cords(map);
+//     ft_printf("player cords: (x: %d, y: %d)\n\n", player_cords->x, player_cords->y);
+//     t_map *flooded = flood_fill(map, player_cords->x, player_cords->y);
+//     str_arr_print(map->map);
+//     ft_printf("\n");
+//     str_arr_print(flooded->map);
+//     if (map)
+//         str_arr_free(map->map);
+//     free(map);
+//     if (flooded)
+//         str_arr_free(flooded->map);
+//     free(flooded);
+//     free(player_cords);
+//     return 0;
+// }
 
 // TODO
 //  make each validator function call another one and on error just print it and exit() no need for main validator function then
