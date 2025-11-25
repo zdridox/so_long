2bc8679e9d08445180d74b40b3d3262f2890b70a
Mateusz <mateusz.zdrod@gmail.com>
2025-11-25 16:11:37 +0100

    clean_shell

--- src/game_control.c
+++ src/game_control.c
@@ -11,17 +11,16 @@ void move_player(t_combo *combo, int x, int y)
     {
         if (combo->map->map[player_pos->y + y][player_pos->x + x] == 'C')
         {
-            combo->data->collectibles++;
-            ft_printf("\rcollected %d/%d\n", combo->data->collectibles, combo->data->collectible_count);
+            ft_printf("\rmoves: %d collected: %d/%d", combo->data->moves_count, ++combo->data->collectibles, combo->data->collectible_count);
         }
         combo->map->map[player_pos->y + y][player_pos->x + x] = 'P';
         combo->map->map[player_pos->y][player_pos->x] = '0';
         render_map(combo->map, combo->data);
-        ft_printf("\rmoves: %d", ++combo->data->moves_count);
+        ft_printf("\rmoves: %d collected: %d/%d", ++combo->data->moves_count, combo->data->collectibles, combo->data->collectible_count);
     }
     if (combo->map->map[player_pos->y + y][player_pos->x + x] == 'E' && combo->data->collectible_count == combo->data->collectibles)
     {
-        ft_printf("\rmoves: %d", ++combo->data->moves_count);
+        ft_printf("\rmoves: %d collected: %d/%d", ++combo->data->moves_count, combo->data->collectibles, combo->data->collectible_count);
         ft_printf("\ngg");
         free(player_pos);
         close_game(combo);
